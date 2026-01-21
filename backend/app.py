80f37eda1f536b40da947d779986853fbf13f964
Usha Nenavath <usha404found@gmail.com>
2026-01-21 15:25:04 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,request
 app=Flask(__name__)
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
+    if request.method=='POST':
+        return redirect(url_for('welcome'))
     return render_template('home.html')
+@app.route("/welcome")
+def welcome():
+    return render_template('welcome.html')
 if __name__=='__main__':
     app.run(debug=True,threaded=True)
 
